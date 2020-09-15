f90a7b0d03bca16e4a1e7324c28b15690d6f33b1
Elleiluj <julie.labbe@polymtl.ca>
2020-09-15 23:13:24 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -10,8 +10,8 @@ def is_even_len(string: str) -> bool:
 
 
 def remove_third_char(string: str) -> str:
-    for string[0,3]
-    pass
+    tring[0,2]
+    
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
