f360b96bba38e9e1fc11c84141e4acc774d599c8
Elleiluj <julie.labbe@polymtl.ca>
2020-09-15 23:43:36 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -23,7 +23,8 @@ def replace_char(string: str, old_char: str, new_char: str) -> str:
    
 
 def get_number_of_char(string: str, char: str) -> int:
-    for i in range(len(string))
+    for i in range(len(string)):
+        if 
 
 
 def get_number_of_words(sentence: str, word: str) -> int:
