62e0420f3d4642fdd492824a85437faea4940dea
Elleiluj <julie.labbe@polymtl.ca>
2020-09-15 23:44:57 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -27,7 +27,7 @@ def get_number_of_char(string: str, char: str) -> int:
     for i in range(len(string)):
         if (string[i]==char):
             count += 1
-        rey
+        return count
 
 
 def get_number_of_words(sentence: str, word: str) -> int:
