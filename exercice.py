b57e88c0c2328d4105d75f05ba66822b1fb4b41c
Elleiluj <julie.labbe@polymtl.ca>
2020-09-15 23:28:40 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -17,8 +17,9 @@ def remove_third_char(string: str) -> str:
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
     for i in string
-        if (string[i] == old_char)
-        return string[:i] + new_char + string[i+1:]
+        if (string[i] == old_char):
+            return string[:i] + new_char + string[i+1:]
+        
    
 
 
