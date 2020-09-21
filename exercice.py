aa401aa232120d21de4242e5d0174135db58d8ae
Elleiluj <julie.labbe@polymtl.ca>
2020-09-21 22:30:24 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -34,8 +34,8 @@ def get_number_of_words(sentence: str, word: str) -> int:
     count = 0
     for i in range(len(sentence)):
         if (sentence[i] == word [j])
-            i +=1 
-            
+            i += 1 
+            j += 1
         for j in range(len(word)):
             if (sentence[i] == word[j])
             count += 1
