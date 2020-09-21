6ed084b776c26c19ca1b048b32e8861d5d025087
Elleiluj <julie.labbe@polymtl.ca>
2020-09-21 22:23:23 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -31,7 +31,7 @@ def get_number_of_char(string: str, char: str) -> int:
 
 
 def get_number_of_words(sentence: str, word: str) -> int:
-    for i in range(len(st)):
+    for i in range(len(senten)):
 
     pass
 
