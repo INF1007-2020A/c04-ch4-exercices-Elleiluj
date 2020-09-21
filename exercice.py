cd56bb63c7e4a0ff7a0ccfe8dcdd7661514c9686
Elleiluj <julie.labbe@polymtl.ca>
2020-09-21 22:45:47 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -34,8 +34,7 @@ def get_number_of_words(sentence: str, word: str) -> int:
     words = sentence.split()
     count = 0
     for chr_sentence in word:
-         for chr_word in word:
-                  if (sentence[i] == word[j]):
+        if (sentence[i] == word[j]):
             count += 1 
     return count
 
